5e63923266734e7b38f0be5ce350fe924c21204b
Excellence Otti <excellenceotti01@gmail.com>
2026-08-26 17:57:20 +0100

    Update AOT navigation links

--- src/data/siteNavigation.js
+++ src/data/siteNavigation.js
@@ -2,9 +2,22 @@
 export const siteNavigation = {
   primary: [
     { id: 'home', label: 'Home', href: '/aot-8/' },
-    { id: 'awards', label: 'Tech Ecosystem Awards', href: 'https://aotlagos.com/' },
-    { id: 'resources', label: 'Resources', items: [{ id: 'media', label: 'Media', href: 'https://aotlagos.com/' }, { id: 'news', label: 'In the news', href: 'https://aotlagos.com/' }] },
-    { id: 'past-editions', label: 'Past editions', items: [{ id: 'archives', label: 'AOT Lagos archives', href: 'https://aotlagos.com/' }] },
+    { id: 'awards', label: 'Tech Ecosystem Awards', href: 'https://aotlagos.com/awards/' },
+    { id: 'resources', label: 'Resources', items: [{ id: 'media', label: 'Media', href: 'https://aotlagos.com/media/' }, { id: 'news', label: 'In the news', href: 'https://aotlagos.com/in-the-news/' }] },
+    {
+      id: 'past-editions',
+      label: 'Past editions',
+      items: [
+        { id: 'edition-1', label: '1.0', href: 'https://aotlagos.com/1-0/' },
+        { id: 'edition-2', label: '2.0', href: 'https://aotlagos.com/2-0/' },
+        { id: 'edition-3', label: '3.0', href: 'https://aotlagos.com/3-0/' },
+        { id: 'edition-4', label: '4.0', href: 'https://aotlagos.com/4-0/' },
+        { id: 'edition-5', label: '5.0', href: 'https://aotlagos.com/5-0/' },
+        { id: 'edition-6', label: '6.0', href: 'https://aotlagos.com/6-0/' },
+        { id: 'edition-7', label: '7.0', href: 'https://aotlagos.com/art-of-technology-lagos/' },
+      ],
+    },
   ],
   registration: { label: 'Register', href: '/aot-8/register/' },
 }
+
